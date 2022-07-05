387034f424ea19b79bd6abafc8243d9e3ea724d6
jupiter <jupiterorbita@users.noreply.github.com>
2022-07-05 11:34:35 -0700

    algos w1d2 bubbleSort

--- ALGOS/w1d2_bubbleSort/bubbleSort.js
+++ ALGOS/w1d2_bubbleSort/bubbleSort.js
@@ -11,13 +11,26 @@
     For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
     -> Assume there are no duplicates
 */
-
+//             i
+//               j
 const nums1 = [5,3,4,2,1];
 const nums2 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 const nums3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 function bubbleSort(nums){
+    // loop over every element
+    for(var i=0; i<nums.length; i++) {
+        for(var j=i+1; j<nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                var temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
 }
 
 // call the function and pass nums1, nums2, nums3
-bubbleSort(nums);
+console.log(nums1);
+bubbleSort(nums1);
+console.log(nums1);
