08f452e086032f7ee43bdbb763b1df02d125b3bd
jupiter <jupiterorbita@users.noreply.github.com>
2022-07-07 14:25:36 -0700

    add w1d4 algos insertion sort

--- ALGOS/w1d4_insertionSort/solution.js
+++ ALGOS/w1d4_insertionSort/solution.js
@@ -30,37 +30,48 @@
     6. move to next item and repeat
 */
 
+function insertionSort2(arr) {
 
-
-
-function insertionSort(arr) {
-    // outside loop start at idx 1
-    for (let x = 1; x < arr.length; x++) {
-        // assign a temp where x is
+    for(var x=1; x<arr.length; x++){
         let temp = arr[x];
-        // inside (backwards) loop start 1 before x
-        for (let y = x - 1; y >= 0; y--) {
-            // check with temp
-            if (arr[y] > temp) {
-                // copy y over +1 idx
-                arr[y + 1] = arr[y];
-                // what if y is at the beginning
-                //     if (y == 0) {
-                //         arr[y] = temp;
-                //     } 
-                // } else {
-                //     // insert temp
-                //     arr[y+1] = temp;
-                //     break;
-                // }
-                arr[y] = temp;
+        // console.log("X: ", x)
+        for(var y=x-1; y>=0; y--){
+            
+            if(arr[y]>temp){
+                arr[y+1] = arr[y];
+                if(y == 0){ 
+                    arr[y] = temp;
+                    // break;
+                }
+            }
+            else {
+                // console.log("else at y:", y)
+                arr[y+1] = temp; 
+                break; //because everything else on the left is already pre-sorted
+            // once we put the temp in we have to break out of its loop; otherwise it will continue 
             }
 
         }
-        
     }
     console.log(arr);
-}
+ }
+
+ function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      // save the current 'target' so this space is available to use for shifting
+      let numToInsert = arr[i];
+      let leftIdx = i - 1;
+  
+      while (leftIdx >= 0 && arr[leftIdx] > numToInsert) {
+        // shift to the right because it's greater than the item we are going to insert
+        arr[leftIdx + 1] = arr[leftIdx];
+        leftIdx--;
+      }
+      // shifting complete, insert position located
+      arr[leftIdx + 1] = numToInsert;
+    }
+    return arr;
+  }
 
 insertionSort([3, 5, 2, 1, 0])
 insertionSort([3, 0])
